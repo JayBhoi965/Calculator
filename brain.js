25ebd2c2c3293f00c59a6c48bc9bee22a13dc39f
Jay Bhoi <154799187+JayBhoi965@users.noreply.github.com>
2025-02-15 05:02:33 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -1,1 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let display = document.querySelector(".display");
+    let buttons = document.querySelectorAll("button");
 
+    let currentInput = "";
+    let operator = "";
+    let previousInput = "";
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+
+            let value = this.innerText;
+
+            if (value === "C") {
+
+
+                currentInput = "";
+                previousInput = "";
+                operator = "";
+                display.innerText = "0";
+
+
+            } else if (value === "⌫") {
+                currentInput = currentInput.slice(0, -1);
+                display.innerText = currentInput || "0";
+            } else if (value === "=") {
+
+                if (previousInput !== "" && currentInput !== "") {
+               
+                    currentInput = eval(previousInput + operator + currentInput);
+                    display.innerText = currentInput;
+                    previousInput = "";
+                    operator = "";
+               
+                }
+                
+            } else if (["+", "-", "×", "÷"].includes(value)) {
+                // Store operator and previous value
+                if (currentInput !== "") {
+                    previousInput = currentInput;
+                    operator = value === "×" ? "*" : value === "÷" ? "/" : value;
+                    currentInput = "";
+                }
+            } else {
+
+                if (currentInput.length < 10) {
+
+                    currentInput += value;
+                    display.innerText = currentInput;
+
+                }
+                
+            }
+        });
+    });
+});
